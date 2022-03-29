69784d2e4d06f636d5a1f468b8533e9ed51c5d72
itu <iturri.jon@gmail.com>
2022-03-29 20:11:25 +0200

    database rows

---
+++ sql/roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user_roles` (`id`, `name`, `permissions`) VALUES
+(1, 'Admin', 'null'),
+(2, 'Compras', 'null'),
+(3, 'Recepción', 'null');
--- sql/settings.sql
+++ sql/settings.sql
@@ -1,1 +1,3 @@
-INSERT INTO `settings` (`id`, `type`, `value`) VALUES (NULL, '0', '1000'), (NULL, '1', '1000');
+INSERT INTO `settings` (`id`, `type`, `value`) VALUES 
+(NULL, '0', '1000'), 
+(NULL, '1', '1000');
