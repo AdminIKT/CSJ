1636f6f1a75e523a2921082180dd5fc071df22b5
aruizfpsanjorge <aruiz@fpsanjorge.com>
2023-01-12 11:52:21 +0100

    Quitar año activo de las configuraciones

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -4,9 +4,11 @@
 @section('content')
 
     @foreach ($collection as $entity)
-        <h6>{{ $entity->getTypeName() }}</h6>
-        <p>{{ $entity->getTypeDescription() }}: <strong>{{ $entity->getValue() }}</strong> <a href="{{ route('settings.edit', ['setting' => $entity->getId()]) }}" class="btn btn-sm btn-outline-secondary mx-2"><span data-feather="edit-2"></span></a></p>
-        <hr>
+        @if($entity->getId() != 1)
+            <h6>{{ $entity->getTypeName() }}</h6>
+            <p>{{ $entity->getTypeDescription() }}: <strong>{{ $entity->getValue() }}</strong> <a href="{{ route('settings.edit', ['setting' => $entity->getId()]) }}" class="btn btn-sm btn-outline-secondary mx-2"><span data-feather="edit-2"></span></a></p>
+            <hr>
+        @endif
     @endforeach
   
 @endsection
