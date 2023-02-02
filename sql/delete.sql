20da62fece8a2799fa62611d19d79174d5981dc8
aruizfpsanjorge <aruiz@fpsanjorge.com>
2023-02-02 13:43:10 +0100

    Script borrado BD

---
+++ sql/delete.sql
@@ -0,0 +1,18 @@
+delete from account_user_rel;
+delete from accounts_drive_files;
+delete from order_products;
+delete from movements;
+delete from actions;
+delete from orders;
+delete from subaccounts;
+delete from accounts;
+delete from supplier_invoiced;
+delete from supplier_incidences;
+delete from supplier_contacts;
+delete from suppliers;
+delete from deparment;
+
+--delete from role_user;
+--delete from settings;
+--delete from user_roles;
+--delete from users;
