5090f47e0c7bea74ca15cdbcd5db78aec8ea4d3f
Iturri <iturri.jon@gmail.com>
2023-02-13 17:52:07 +0100

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,6 +52,7 @@ class UserController extends BaseController
                                 ]), $ppg);
 
         return view('users.show', [
+            'perPage'    => $ppg,
             'entity'     => $user,
             'collection' => $accounts,
         ]); 
